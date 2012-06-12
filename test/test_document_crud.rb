23ccdcb6b06ed31b481a5ac3f1d7739a745afa16
veny <vaclav.sykora@gmail.com>
2012-06-13 00:07:54 +0200

    progress on document CR

---
+++ test/test_document_crud.rb
@@ -0,0 +1,43 @@
+require 'test/unit'
+require 'orientdb4r'
+
+###
+# This class tests Data Manipulation Operarions.
+class TestDmo < Test::Unit::TestCase
+  include Orientdb4r::Utils
+
+  CLASS = 'testing'
+  DB = 'temp'
+  Orientdb4r::logger.level = Logger::DEBUG
+
+
+  def setup
+    @client = Orientdb4r.client
+    @client.connect :database => DB, :user => 'admin', :password => 'admin'
+    @client.create_class(CLASS) do |c|
+      c.property 'prop1', :integer
+      c.property 'prop2', :string, :mandatory => true, :notnull => :true, :min => 1, :max => 99
+    end
+  end
+
+  def teardown
+    # remove the testing class after each test
+    @client.drop_class(CLASS)
+    @client.disconnect
+  end
+
+
+  ###
+  # GET
+  def test_get_document
+    rid = @client.create_document( { '@class' => CLASS, 'prop1' => 1, 'prop2' => 'text' })
+
+    doc = @client.get_document rid
+    assert_equal CLASS, doc.doc_class
+    assert_equal rid, doc.doc_rid
+    assert_equal 0, doc.doc_version
+    assert_equal 'd', doc.doc_type
+  end
+
+
+end
