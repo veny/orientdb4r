531c8ee956bf6fa02841e48c1226c71c27ff6d56
veny <vaclav.sykora@gmail.com>
2015-03-05 21:05:20 +0100

    Enh #36

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 require 'test/unit'
 require 'coveralls'
+Coveralls.wear!
 
 $: << File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'orientdb4r'
 
 Orientdb4r::logger.level = Logger::FATAL
-Coveralls.wear!
