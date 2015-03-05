cce2fb53cf59215d816059b82d610c4025bd46cd
veny <vaclav.sykora@gmail.com>
2015-03-05 14:04:06 +0100

    working on Travis support

--- ci/initialize-ci.sh
+++ ci/initialize-ci.sh
@@ -3,13 +3,20 @@
 PARENT_DIR=$(dirname $(cd "$(dirname "$0")"; pwd))
 CI_DIR="$PARENT_DIR/ci/environment"
 
+#CI_DIR=/tmp/travis
+#mkdir -p $CI_DIR
+#exit
+
 ODB_VERSION=${1:-"2.0.4"}
 ODB_DIR="${CI_DIR}/orientdb-community-${ODB_VERSION}"
 ODB_LAUNCHER="${ODB_DIR}/bin/server.sh"
 
 echo "=== Initializing CI environment ==="
+echo $(which java)
 
 cd "$PARENT_DIR"
 
 echo "--- Downloading OrientDB v${ODB_VERSION} ---"
-wget "http://www.orientechnologies.com/download.php?email=unknown@unknown.com&file=orientdb-community-${ODB_VERSION}.tar.gz&os=linux
+wget "http://www.orientechnologies.com/download.php?email=unknown@unknown.com&file=orientdb-community-${ODB_VERSION}.tar.gz&os=linux" -O $CI_DIR/orientdb-community-${ODB_VERSION}.tar.gz
+echo "--- Unpacking ---------------------"
+tar xf $CI_DIR/orientdb-community-${ODB_VERSION}.tar.gz -C $CI_DIR
