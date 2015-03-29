c6da473195a7f4641875725e6a282b51fdeeda9d
veny <vaclav.sykora@gmail.com>
2015-03-29 23:12:02 +0200

    bin protocol

---
+++ lib/orientdb4r/bin/constants.rb
@@ -0,0 +1,20 @@
+module Orientdb4r
+
+  module Binary
+
+    module Constants
+
+      MINIMAL_PROTOCOL_VERSION = 28 # ODB v2.0.5
+
+      REQUEST_SHUTDOWN         =  1
+      REQUEST_CONNECT          =  2
+      REQUEST_DB_OPEN          =  3
+
+      STATUS_OK      = 0
+      STATUS_ERROR   = 1
+
+    end
+
+  end
+
+end
